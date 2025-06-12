e71a613c66c0ea75b275fe4a37b82936aa95814a
victorcs85 <victor.costa.santiago@gmail.com>
2025-06-12 10:58:20 -0300

    feat: Adding some modules to koin DI file.

--- app/src/main/java/br/com/victorcs/trellochallenge/di/TrelloInitialization.kt
+++ app/src/main/java/br/com/victorcs/trellochallenge/di/TrelloInitialization.kt
@@ -1,28 +1,61 @@
 package br.com.victorcs.trellochallenge.di
 
+import br.com.victorcs.trellochallenge.core.providers.IDispatchersProvider
+import br.com.victorcs.trellochallenge.core.providers.IDispatchersProviderImpl
+import br.com.victorcs.trellochallenge.core.services.WifiService
+import br.com.victorcs.trellochallenge.data.dto.BoardItemDto
+import br.com.victorcs.trellochallenge.data.mapper.BoardItemMapper
+import br.com.victorcs.trellochallenge.data.repository.BoardsRepositoryImpl
+import br.com.victorcs.trellochallenge.data.source.remote.RetrofitConfig
+import br.com.victorcs.trellochallenge.data.source.remote.TrelloService
+import br.com.victorcs.trellochallenge.data.source.remote.inteceptor.ConnectivityInterceptor
+import br.com.victorcs.trellochallenge.domain.mapper.DomainMapper
+import br.com.victorcs.trellochallenge.domain.model.BoardItem
+import br.com.victorcs.trellochallenge.domain.repository.IBoardsRepository
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
-class TrelloInitialization: ModuleInitialization() {
+private const val TRELLO_BASE_URL = "https://api.trello.com/1/members/me/"
 
-    //region Data Module
-    private val dataModule = module {
+class TrelloInitialization : ModuleInitialization() {
+
+    //region Remote Module
+    private val remoteModule = module {
+        single { WifiService(context = androidContext()) }
+        single<IDispatchersProvider> { IDispatchersProviderImpl() }
+        single { ConnectivityInterceptor(wifiService = get()) }
+        single {
+            RetrofitConfig.create(
+                service = TrelloService::class.java,
+                baseUrl = TRELLO_BASE_URL,
+                wifiService = get(),
+            )
+        }
     }
     //endregion
 
-    //region Domain Module
-    private val domainModule = module {
+    //region Mapper Module
+    private val mapperModule = module {
+        single<DomainMapper<BoardItemDto, BoardItem>> { BoardItemMapper() }
+    }
+    // endregion
+
+    //region Repository Module
+    private val repositoryModule = module {
+        single<IBoardsRepository> { BoardsRepositoryImpl( service = get(), mapper = get()) }
     }
     //endregion
 
-    //region Presentation Module
-    private val presentationModule = module {
+    //region ViewModel Module
+    private val viewModelModule = module {
     }
     //endregion
 
     override fun init(): List<Module> = listOf(
-        dataModule,
-        domainModule,
-        presentationModule
+        remoteModule,
+        mapperModule,
+        repositoryModule,
+        viewModelModule
     )
 }
